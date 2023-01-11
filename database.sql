6a519aac30c565d403b7ea2e77918a1e89f7f9b9
jerempjerem <jerem830@icloud.com>
2023-01-11 13:32:04 +0100

    Added db

---
+++ database.sql
@@ -0,0 +1,33 @@
+-- Creation de la table des AUTEURS
+CREATE TABLE "AUTEURS" (
+	"AuteurID"	INTEGER,
+	"Nom"	TEXT NOT NULL,
+	PRIMARY KEY("AuteurID" AUTOINCREMENT)
+);
+
+-- Creation de la table des CORRIGES
+CREATE TABLE "CORRIGES" (
+	"CorrigeID"	INTEGER,
+	"Fichier"	TEXT NOT NULL,
+	"AuteurID_ext"	INTEGER NOT NULL,
+	PRIMARY KEY("CorrigeID" AUTOINCREMENT)
+);
+
+-- Creation de la table des KEYWORDS
+CREATE TABLE "KEYWORDS" (
+	"KeywordID"	INTEGER,
+	"Keyword"	TEXT NOT NULL,
+	PRIMARY KEY("KeywordID" AUTOINCREMENT)
+);
+
+-- Creation de la table des EXERCICES
+CREATE TABLE "EXERCICES" (
+	"ExercicesID"	INTEGER,
+	"Fichier"	TEXT NOT NULL,
+	"Ref_exo"	TEXT,
+	"Difficulte"	TEXT NOT NULL,
+	"AuteurID_ext"	INTEGER NOT NULL,
+	"CorrigeID_ext"	INTEGER,
+	"KeywordID_ext"	INTEGER,
+	PRIMARY KEY("ExercicesID" AUTOINCREMENT)
+);
